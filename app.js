1d5bf4014aa41ef78a5f8361306250a2dd37b042
achmadss <anakinskywalk1@gmail.com>
2024-12-15 10:01:54 +0700

    update app

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 import express from 'express';
-import { hostname as _hostname } from 'os';
+// import { hostname as _hostname } from 'os';
 
 const app = express();
 const port = 3000;
 
 // App endpoint
 app.get('/', (req, res) => {
-    const hostname = _hostname(); // Pod name
-    const worker = process.env.WORKER || 'Unknown Worker'; // Node hostname passed as an environment variable
-    res.send(`<h1>Hello! I'm from ${worker} on pod ${hostname}</h1>`);
+    // const hostname = _hostname(); // Pod name
+    // const worker = process.env.WORKER || 'Unknown Worker'; // Node hostname passed as an environment variable
+    // res.send(`<h1>Hello! I'm from ${worker} on pod ${hostname}</h1>`);
+    res.send(`<h1>hello world</h1>`)
 });
 
 // Start the server
